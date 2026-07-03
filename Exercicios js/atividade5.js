faa3c0c432954705b04bca155fca4a07a22f2874
VICTOR GABRIEL RAMOS <victor_ramos150@estudante.sesisenai.org.br>
2026-07-03 16:47:43 -0300

    Exercicio 5

---
+++ Exercicios js/atividade5.js
@@ -0,0 +1,5 @@
+const primeiroNome = "João";                   // O _ está Errado.
+const dataNascimento = "01/01/2000";           // Começa com letra minúscula.
+const salario = 3000;                          // Deveria ser tudo minúsculo.
+let numeroDeTentativas = 0;                    // Deveria a cada palavra diferente ter a primeira letra maiúscula.
+const nomeCompleto = "João Silva";             // Deveria começar com a letra minúscula e na próxima palavra ter a maiúscula.
