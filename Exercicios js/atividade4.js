d30e8998ae2ee0110a5ded25173027880e690008
VICTOR GABRIEL RAMOS <victor_ramos150@estudante.sesisenai.org.br>
2026-07-03 16:33:47 -0300

    Exercicio 3 e 4

---
+++ Exercicios js/atividade4.js
@@ -0,0 +1,6 @@
+console.log(5 == "5");            //true      o 5 realmente é igual a 5.
+console.log(5 === "5");           //false     o 5 até é 5, mas seu tipo é diferente.
+console.log(0 == false);          //true      o 0 é a mesma coisa que false por não ser nada.
+console.log(0 === false);          //false    eles são de tipos diferentes.
+console.log(null == undefined);    //true     ambos são sem valor.
+console.log(null === undefined);   //false    eles são de tipos diferentes.
