4d7fd76b7806feb0d40031b5cabdd87345f77c9a
VICTOR GABRIEL RAMOS <victor_ramos150@estudante.sesisenai.org.br>
2026-07-03 17:15:47 -0300

    Desafio

---
+++ Exercicios js/desafio.js
@@ -0,0 +1,8 @@
+const nome = "XubireiBirei";
+let classe = "Aventureiro";
+let nivel = 55;
+let pontosDeVida = 7000;
+let vivo = true;
+let armaEquipada = "Espada de Ki";
+
+console.log(`${nome} é um ${classe} nível ${nivel} com ${pontosDeVida} HP. Arma: ${armaEquipada}. Status: ${vivo}`);
