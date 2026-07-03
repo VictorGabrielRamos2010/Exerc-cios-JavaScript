e22eca19c6c3b82e1e2e5be1efa355109211024a
VICTOR GABRIEL RAMOS <victor_ramos150@estudante.sesisenai.org.br>
2026-07-03 16:18:49 -0300

    Exercicio js

---
+++ Exercicios/atividade1.js
@@ -0,0 +1,8 @@
+const nomeUm = "Ana";
+const nomeDois = "Carlos";
+
+let idadeUm = 25;
+let idadeDois = 30;
+
+console.log(nomeDois, idadeDois);
+
---
+++ Exercicios/atividade2.js
@@ -0,0 +1,11 @@
+let int = 10;
+let dec = 1.234;
+let nome = "Victor Gabriel Ramos";
+let booleano = true;
+let undefined
+
+console.log(int);
+console.log(dec);
+console.log(nome);
+console.log(booleano);
+console.log(undefined);
