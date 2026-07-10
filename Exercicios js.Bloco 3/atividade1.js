d0b47ff8970c3b1fff4986da28366331f386687b
VICTOR GABRIEL RAMOS <victor_ramos150@estudante.sesisenai.org.br>
2026-07-10 16:35:42 -0300

    Exercicio 1

---
+++ Exercicios js.Bloco 3/atividade1.js
@@ -0,0 +1,17 @@
+function saudacao(nome) {
+    return "Olá, " + nome + "! Bem-vindo ao sistema.";
+}
+console.log(saudacao("Victor Gabriel Ramos"));
+
+function ehPositivo(numero) {
+    return numero > 0;
+}
+console.log(ehPositivo(10));
+console.log(ehPositivo(-5));
+console.log(ehPositivo(0));
+
+function calcularArea(largura, altura) {
+    return largura * altura;
+}
+console.log(calcularArea(5, 4));
+console.log(calcularArea(10, 2));
